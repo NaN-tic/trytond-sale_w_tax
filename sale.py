50f979b8b751098b629a32313ea4f9058a23345f
Raimon Esteve <raimon@nan-tic.com>
2018-11-12 14:33:05 +0100

    FIX compute amount with tax with multiples taxes

--- sale.py
+++ sale.py
@@ -65,12 +65,13 @@ class SaleLine:
         unit_price_w_tax = {}
 
         def compute_amount_with_tax(line):
-            tax_list = Tax.compute(line.taxes,
-                line.unit_price or Decimal('0.0'),
-                line.quantity or 0.0)
-            tax_amount = sum([t['amount'] for t in tax_list], Decimal('0.0'))
-            amount = sum([t['base'] for t in tax_list], Decimal('0.0'))
-            return amount + tax_amount
+            tax_amount = Decimal('0.0')
+            if line.taxes:
+                tax_list = Tax.compute(line.taxes,
+                    line.unit_price or Decimal('0.0'),
+                    line.quantity or 0.0)
+                tax_amount = sum([t['amount'] for t in tax_list], Decimal('0.0'))
+            return line.amount + tax_amount
 
         for line in lines:
             amount = Decimal('0.0')
